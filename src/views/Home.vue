cfd5eb7f6755f7ac90e4a14493400ccd27f1eee4
fendy <2457037852@qq.com>
2021-08-26 21:27:19 +0800

    topnav样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
      <div class="topnav">
-         <div class="log"></div>
-         <div class="menu"></div>
+         <div class="logo">LOGO</div>
+         <div class="menu">
+             <ul class="menu">
+                 <li>菜单1</li>
+                 <li>菜单2</li>
+             </ul>
+         </div>
      </div>
      <div class="banner"> 
          <h1>轱辘UI</h1>
@@ -15,6 +20,24 @@
     </div>
 </template>
 <style lang="scss" scoped>
-.topnar{border:1px solid red;}
+.topnav{
+    background:pink;
+    display:flex;
+    padding:16px;
+    > .logo{
+        max-width:6em;
+        margin-right:auto;
+    }
+    > .menu{
+        display:flex;
+        white-space:nowrap;
+        flex-wrap:noewrap;
+        > li{
+            margin:0 1em;
+        }
+
+
+    }
+}
     
 </style>
