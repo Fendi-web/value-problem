670f242cf575f4d1e5bc188e00c03e200054ef6c
fendy <2457037852@qq.com>
2021-08-27 06:51:29 +0800

    初始化aside

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,13 +1,37 @@
 <template>
     <div>
+        <Topnav/>
     <div class="topnav">
          <div class="log"></div>
          <div class="menu"></div>
      </div> 
      <div class="content">
-         <aside>边栏</aside>
+         <aside>
+             <h2>组建列表</h2>
+             <ol>
+                 <li>
+                     <router-link to="/doc/switch">switch 组件</router-link>
+                 </li>
+                 <li>
+                     <router-link to="/doc/button">Button 组件</router-link>
+                 </li>
+                 <li>
+                     <router-link to="/doc/dialog">Dialog 组件</router-link>
+                 </li>
+                 <li>
+                     <router-link to="/doc/tabs">Tabs 组件</router-link>
+                 </li>
+             </ol>
+         </aside>
          <main>主内容</main>
 
      </div> 
     </div>
 </template>
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+export default {
+    components:{Topnav}
+    
+}
+</script>
